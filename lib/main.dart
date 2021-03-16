6b56ad254c97c228aa1f72580c9784ed1496759b
Tooba Tehreem Sheikh <toobatehreemsheikh@gmail.com>
2021-03-16 11:33:34 +0500

    ch2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,57 @@ class MyApp extends StatelessWidget {
         title: Text('Hi flutter'),
         backgroundColor: Colors.teal[800],
       ),
-      body: Container(
-        child: Text(
-          'Hi',
-          overflow: TextOverflow.ellipsis,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            color: Colors.amber,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width / 4,
+                    height: MediaQuery.of(context).size.height / 3,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Hello',
+                      style: TextStyle(
+                          fontFamily: 'Times New Roman', color: Colors.white),
+                    ),
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width / 4,
+                    height: MediaQuery.of(context).size.height / 3,
+                    child: Text(
+                      'Hello1',
+                      style: TextStyle(
+                          fontFamily: 'Times New Roman', color: Colors.white),
+                    ),
+                    color: Colors.red,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width / 4,
+                    height: MediaQuery.of(context).size.height / 3,
+                    child: Text(
+                      'Hello2',
+                      style: TextStyle(
+                          fontFamily: 'Times New Roman', color: Colors.white),
+                    ),
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
